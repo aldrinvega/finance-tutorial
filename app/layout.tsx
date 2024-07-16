17d568bcdc89a18e78705fa1955c0a9a48ffbf9c
Aldrin Vega <aldrinmundovega@gmail.com>
2024-07-16 23:32:41 +0800

    add: sign in and sign up page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +23,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <ClerkProvider>
+        <body className={inter.className}>{children}</body>
+      </ClerkProvider>
+      
     </html>
   );
 }
